c29fe574bbde4b9fc32b15df420a243e4cf0065a
Tales Pereira <42183799+talesdsp@users.noreply.github.com>
2020-06-06 02:57:02 -0300

    define field methods

--- lib/models/field.dart
+++ lib/models/field.dart
@@ -1,3 +1,4 @@
+import 'package:campo/models/explosion_exception.dart';
 import 'package:flutter/foundation.dart';
 
 class Field {
@@ -33,5 +34,58 @@ class Field {
 
   int get countBombsInNeighborhood {
     return neighborFields.where((field) => field.hasBomb).length;
+  }
+
+  // reset all fields to re-start the game
+  void reset() {
+    _isOpen = false;
+    _hasFlag = false;
+    _hasBomb = false;
+    _exploded = false;
+  }
+
+  // add closest fields to 'neighborhood'
+  void addNeighbor(Field field) {
+    final deltaRow = (row - field.row).abs();
+    final deltaCol = (col - field.col).abs();
+
+    if (deltaRow == 0 && deltaCol == 0) return;
+
+    if (deltaRow <= 1 && deltaCol <= 1) {
+      neighborFields.add(field);
+    }
+  }
+
+  // open field
+  void open() {
+    if (isOpen) return;
+
+    _isOpen = true;
+
+    if (hasBomb) {
+      _exploded = true;
+      throw ExplosionException();
+    }
+
+    if (safeNeighborhood) {
+      neighborFields.forEach((field) => field.open());
+    }
+  }
+
+  // add (or remove) flag dinamically
+  void toggleFlag() {
+    _hasFlag = !_hasFlag;
+  }
+
+  // add bomb to this field
+  void addBomb() {
+    _hasBomb = true;
+  }
+
+  // show this field's bomb (if has any)
+  void showBomb() {
+    if (hasBomb) {
+      _isOpen = true;
+    }
   }
 }
