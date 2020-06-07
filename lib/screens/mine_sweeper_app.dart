d684b942686456993710146ddc2a80c5811a8563
Tales Pereira <42183799+talesdsp@users.noreply.github.com>
2020-06-06 23:05:35 -0300

    create main screen

---
+++ lib/screens/mine_sweeper_app.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:mine_sweeper/components/board_widget.dart';
+import 'package:mine_sweeper/components/result_widget.dart';
+import 'package:mine_sweeper/models/board.dart';
+import 'package:mine_sweeper/models/explosion_exception.dart';
+import 'package:mine_sweeper/models/field.dart';
+
+class MineSweeperApp extends StatefulWidget {
+  @override
+  _MineSweeperAppState createState() => _MineSweeperAppState();
+}
+
+class _MineSweeperAppState extends State<MineSweeperApp> {
+  bool _win;
+  Board _board;
+
+  void _reset() {
+    setState(() {
+      _win = null;
+      _board.restart();
+    });
+  }
+
+  void _onOpen(Field field) {
+    if (_win != null) return;
+    setState(() {
+      try {
+        field.open();
+        if (_board.resolved) {
+          _win = true;
+        }
+      } on ExplosionException {
+        _win = false;
+        _board.showAllBombs();
+      }
+    });
+  }
+
+  void _onToggleFlag(Field field) {
+    if (_win != null) return;
+    setState(() {
+      field.toggleFlag();
+      if (_board.resolved) {
+        _win = true;
+      }
+    });
+  }
+
+  Board _getBoard(double width, double height) {
+    if (_board == null) {
+      int cols = 15;
+      double fieldSize = width / cols;
+      int rows = (height / fieldSize).floor();
+
+      _board = Board(
+        rows: rows,
+        cols: cols,
+        countBombs: 1,
+      );
+    }
+
+    return _board;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue[50],
+        appBar: ResultWidget(win: _win, onReset: _reset),
+        body: Container(
+          child: LayoutBuilder(
+            builder: (ctx, constraints) {
+              return BoardWidget(
+                board: _getBoard(constraints.maxWidth, constraints.maxHeight),
+                onOpen: _onOpen,
+                onToggleFlag: _onToggleFlag,
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
