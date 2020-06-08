9f30dfbe57041b90e0245aa7cd5a3a2a844b4414
Tales Pereira <42183799+talesdsp@users.noreply.github.com>
2020-06-08 20:58:53 -0300

    enable routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
-
-import 'screens/mine_sweeper_app.dart';
+import 'package:mine_sweeper/screens/game_mode.dart';
+import 'package:mine_sweeper/screens/mine_sweeper.dart';
+import 'package:mine_sweeper/screens/score.dart';
 
 void main() {
-  runApp(MineSweeperApp());
+  runApp(
+    MaterialApp(
+      initialRoute: GameModeScreen.routeName,
+      routes: {
+        GameModeScreen.routeName: (context) => GameModeScreen(),
+        MineSweeperScreen.routeName: (context) => MineSweeperScreen(),
+        ResultScreen.routeName: (context) => ResultScreen(),
+      },
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        buttonTheme: ButtonThemeData(
+          disabledColor: Colors.red[50],
+        ),
+      ),
+    ),
+  );
+  // runApp(MineSweeperApp());
 }
