dddfb3fa2f539ca30594ceac87d11eabb35117e5
Tales Pereira <42183799+talesdsp@users.noreply.github.com>
2020-06-08 22:48:43 -0300

    add tool to scale size safely

--- lib/config/size_config.dart
+++ lib/config/size_config.dart
@@ -2,16 +2,37 @@ import 'package:flutter/widgets.dart';
 
 class SizeConfig {
   static MediaQueryData _mediaQueryData;
+
   static double screenWidth;
   static double screenHeight;
+
+  /// Recommended for scaling blocks
   static double blockSizeHorizontal;
   static double blockSizeVertical;
 
+  static double _safeAreaHorizontal;
+  static double _safeAreaVertical;
+
+  ///  Recommended for scaling texts
+  static double safeBlockHorizontal;
+  static double safeBlockVertical;
+
   void init(BuildContext context) {
     _mediaQueryData = MediaQuery.of(context);
+
     screenWidth = _mediaQueryData.size.width;
     screenHeight = _mediaQueryData.size.height;
+
     blockSizeHorizontal = screenWidth / 100;
     blockSizeVertical = screenHeight / 100;
+
+    _safeAreaHorizontal =
+        _mediaQueryData.padding.left + _mediaQueryData.padding.right;
+
+    _safeAreaVertical =
+        _mediaQueryData.padding.top + _mediaQueryData.padding.bottom;
+
+    safeBlockHorizontal = (screenWidth - _safeAreaHorizontal) / 100;
+    safeBlockVertical = (screenHeight - _safeAreaVertical) / 100;
   }
 }
