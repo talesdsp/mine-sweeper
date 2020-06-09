68c018d5ca4f4f530eb0b2e29d8231888864ba82
Tales Pereira <42183799+talesdsp@users.noreply.github.com>
2020-06-08 22:49:11 -0300

    :fire: remove unused lines

--- lib/components/result_widget.dart
+++ lib/components/result_widget.dart
@@ -4,8 +4,6 @@ class ResultWidget extends StatelessWidget implements PreferredSizeWidget {
   final bool win;
   final Function onReset;
 
-  bool _hover = false;
-
   ResultWidget({
     @required this.win,
     @required this.onReset,
@@ -22,9 +20,6 @@ class ResultWidget extends StatelessWidget implements PreferredSizeWidget {
   }
 
   IconData _getIcon() {
-    if (_hover) {
-      return Icons.sentiment_neutral;
-    }
     if (win == null) {
       return Icons.sentiment_satisfied;
     } else if (win) {
@@ -37,45 +32,54 @@ class ResultWidget extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.grey,
+      color: Colors.blue,
       child: SafeArea(
-        child: Container(
-          padding: EdgeInsets.all(10),
-          child: CircleAvatar(
-            backgroundColor: _getCor(),
-            child: InkWell(
-              child: IconButton(
-                padding: EdgeInsets.all(0),
-                icon: Icon(
-                  _getIcon(),
-                  color: Colors.black,
-                  size: 35,
-                ),
-                onPressed: () {
-                  confirmRestartDialog(context, win);
-                },
-              ),
-              onHover: (b) => {},
+        child: Row(
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('ola'),
+              ],
             ),
-          ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundColor: _getCor(),
+                  child: InkWell(
+                    child: IconButton(
+                      padding: EdgeInsets.all(0),
+                      icon: Icon(
+                        _getIcon(),
+                        color: Colors.black,
+                        size: 35,
+                      ),
+                      onPressed: () {
+                        confirmRestartDialog(context, win);
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
 
-  confirmRestartDialog(BuildContext context, bool win) {
+  void confirmRestartDialog(BuildContext context, bool win) {
     // set up the buttons
     Widget cancelButton = FlatButton(
       child: Text("Cancel"),
-      onPressed: () {
-        Navigator.of(context).pop();
-      },
+      onPressed: () {},
     );
     Widget restartButton = RaisedButton(
         child: Text("Restart"),
         onPressed: () {
-          Navigator.of(context).pop();
           onReset();
+          Navigator.of(context, rootNavigator: true).pop();
         });
 
     Text _getText() {
@@ -88,6 +92,7 @@ class ResultWidget extends StatelessWidget implements PreferredSizeWidget {
     // set up the AlertDialog
     AlertDialog alert = AlertDialog(
       title: Text('Restart game'),
+      backgroundColor: Colors.blueAccent,
       content: _getText(),
       actions: [
         cancelButton,
